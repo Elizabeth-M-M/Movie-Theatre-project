b9cd1c0e1368b525a838cb8c930f1f8149cda9e8
Elizabeth <mwendemuthusi6@gmail.com>
2022-12-09 14:09:44 +0300

    added js to display data from db.json

---
+++ script.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', () => {
+    getMovies();
+})
+function getMovies() {
+    fetch('http://localhost:3000/movies').then(resp =>resp.json()).then(movies => movies.forEach(movie=>displayMovies(movie)))
+}
+function displayMovies(movie) {
+    
+    let container = document.querySelector('.main-content-movie-container');
+    let card = document.createElement('div');
+    card.className = "card";
+    card.innerHTML = `
+    <div class="card-img-container">
+        <img src="${movie.poster}" alt="${movie.title}">
+        <h5>${movie.title}</h5>
+    </div>
+    // <div class="overlayDet">
+    //     <p>${movie.description}</p>
+    //     <button>More Info</button>
+    // </div>`
+    container.append(card);
+
+    // sort display hover
+    // let imgContainer = document.querySelector('.card .card-img-container');
+    // container.addEventListener('mouseenter', (event) => {
+    //     console.log('hey')
+    //     console.log(event.target)
+    // })
+}

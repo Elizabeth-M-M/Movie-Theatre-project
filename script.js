802c95bb78929240dfc88f81774493cf3baf982f
Elizabeth <mwendemuthusi6@gmail.com>
2022-12-10 16:08:48 +0300

    fixed the sold out button

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     getMovies();
 
 })
+// GET data to display to screen
 function getMovies() {
     fetch('http://localhost:3000/movies').then(resp => resp.json()).then(movies => {
         movies.forEach(movie => displayMovies(movie))
@@ -9,39 +10,41 @@ function getMovies() {
 }
 
 function displayMovies(movie) {
-    let images = document.querySelectorAll(".img-container.img-size");    
+    // Div containing images on load screen
+    let images = document.querySelectorAll(".img-container.img-size");
+    // Creating a list containing all the movie titles
     let ul = document.querySelector('#list');
     let li = document.createElement('li');
     li.innerHTML = movie.title;
     ul.appendChild(li);
+    // Targeting list items to display items when clicked
     li.addEventListener('click', (event) => {
         if (event.target.innerHTML === movie.title) {
             displayMovieChange(movie)
         }
     })
+    // Targeting images on the loading screen to display movies when clicked
     images.forEach((img) => {
       img.addEventListener("click", (event) => {
-        //   console.log(event.target.parentElement.id);
-        //   console.log(movie.id);
         if (event.target.parentElement.id === movie.id) {
           displayMovieChange(movie);
         }
       });
-    }); 
+    });
 }
+// Actual display after one of two events above; image or list item clicked
 function displayMovieChange(selectedMovie) {
+    // div to embed the html contents on the side
     let containerContents = document.querySelector('.container-dits');
-
+    // Calculating tickets available for sale. assumption is that the movie theatre full capacity is 30 seats
     let ticketsAvail = selectedMovie.capacity - selectedMovie.tickets_sold;
-    console.log(ticketsAvail)
 
     if (ticketsAvail<1) {
         ticketsAvail = 0;
     } else {
        ticketsAvail = selectedMovie.capacity - selectedMovie.tickets_sold;
     }
-
-
+// Displaying contents
     containerContents.innerHTML = `
             <div class="container-img">
                 <img src="${selectedMovie.poster}" alt="${selectedMovie.title}">
@@ -59,29 +62,23 @@ function displayMovieChange(selectedMovie) {
                 </div>
             </div>
                     `;
-
+// Buying tickets
    let buy = document.querySelector('#buyTicket');
+   if(ticketsAvail<1){
+    buy.innerHTML = "Sold Out";
+    buy.classList.add('allout');
+   }
     buy.addEventListener('click', (event) => {
         if (ticketsAvail < 1) {
-            buy.style.backgroundColor = "red";
-            buy.style.color = "white";
-            buy.innerHTML = "Sold Out";
-            selectedMovie.tickets_sold=0
+            selectedMovie.tickets_sold = 0
         } else {
-            console.log(selectedMovie.tickets_sold)
-       parseInt(selectedMovie.tickets_sold, 10)
-       selectedMovie.tickets_sold++
-
-
-        console.log("sold"+selectedMovie.tickets_sold)
-        ticketsAvail = selectedMovie.capacity - selectedMovie.tickets_sold;
-        document.querySelector("#ticketNumber span").innerHTML = ticketsAvail;
-        console.log(typeof selectedMovie.tickets_sold)
-       updateServerOnTicketsAvail(selectedMovie)
+            parseInt(selectedMovie.tickets_sold, 10)
+            selectedMovie.tickets_sold++;
+            ticketsAvail = selectedMovie.capacity - selectedMovie.tickets_sold;
+            document.querySelector("#ticketNumber span").innerHTML = ticketsAvail;
+            updateServerOnTicketsAvail(selectedMovie)
              }
-
-
-    })
+        })
     }
 
 
